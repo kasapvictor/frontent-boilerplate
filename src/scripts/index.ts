6153db7f187950d05590a3ed81c4367e894e87b5
Victor Kasap <victorkasap@Galactus.local>
2022-05-22 09:41:21 +0400

    feat: Updated eslint config

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,7 +1,10 @@
 import '../scss/styles.scss';
 
 import Button from './components/Button';
+import HomePage from './pages/home';
 
 Button({ containerName: 'container__button', text: '2new 2button' });
 
-console.log('INDEX.JS');
+HomePage();
+
+console.log('check eslint');
