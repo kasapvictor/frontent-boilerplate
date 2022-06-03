203215102cada1a6e72d53407b1c6d94e1b7bad5
Victor Kasap <victorkasap@Galactus.local>
2022-06-03 08:17:11 +0400

    upd: Updated README.md

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const browserSync = require('browser-sync').create();
 browserSync.init({
     watch: true,
     server: "./src",
-    port: 4321,
+    port: 4444,
     open: false,
     notify: false,
     files: "./src/**/*",
