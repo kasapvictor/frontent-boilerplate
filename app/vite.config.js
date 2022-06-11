5bd98e382f3243822bbe5cf3fec38ed29b7f0782
Victor Kasap <victorkasap@Galactus.local>
2022-06-11 17:25:55 +0400

    upd: Updated VITE config

--- app/vite.config.js
+++ app/vite.config.js
@@ -27,19 +27,19 @@ export default defineConfig(() => ({
           if (/\.css$/i.test(name ?? "")) {
             return "css/styles.css";
           }
-
+          
           if (/\.(png|jpe?g|gif|webp)$/i.test(name ?? "")) {
             return "images/[name][extname]";
           }
-
+          
           if (/\.(svg)$/i.test(name ?? "")) {
             return "svg/[name][extname]";
           }
-
+          
           if (/\.(woff(2)?|ttf|eot)$/i.test(name ?? "")) {
             return "fonts/[name][extname]";
           }
-
+          
           // default value
           // ref: https://rollupjs.org/guide/en/#outputassetfilenames
           return "[ext]/[name][extname]";
@@ -48,13 +48,12 @@ export default defineConfig(() => ({
       input: [
         resolve(__dirname, "src/index.html"),
         resolve(__dirname, "src/page1.html")
-      ],
+      ]
     }
   },
   css: {
-    devSourcemap: true,
-  },
-  
+    devSourcemap: true
+  }
 }));
 
 // https://vitejs.dev/config
