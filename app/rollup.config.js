4083e47bfe6d4be920e3f90c14e162bc6ae0f695
Victor Kasap <victorkasap@Galactus.local>
2022-06-12 10:46:20 +0400

    feat: Change generic for dom node

--- app/rollup.config.js
+++ app/rollup.config.js
@@ -2,10 +2,7 @@ import { resolve } from 'path';
 import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 
 export default {
-  input: [
-    resolve(__dirname, 'src/index.html'),
-    resolve(__dirname, 'src/page1.html')
-  ],
+  input: [resolve(__dirname, 'src/index.html'), resolve(__dirname, 'src/page1.html')],
   output: {
     entryFileNames: 'index.js',
     chunkFileNames: 'js/[name].js',
@@ -13,22 +10,22 @@ export default {
       if (/\.css$/i.test(name ?? '')) {
         return 'css/styles.css';
       }
-      
+
       if (/\.(png|jpe?g|gif|webp)$/i.test(name ?? '')) {
         return 'images/[name][extname]';
       }
-      
+
       if (/\.(svg)$/i.test(name ?? '')) {
         return 'svg/[name][extname]';
       }
-      
+
       if (/\.(woff(2)?|ttf|eot)$/i.test(name ?? '')) {
         return 'fonts/[name][extname]';
       }
-      
+
       return '[ext]/[name][extname]';
     },
-    format: 'esm' // 'esm' ||'cjs'
+    format: 'esm', // 'esm' ||'cjs'
   },
   plugins: [
     getBabelOutputPlugin({
--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,7 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
-import rollupConfig from "./rollup.config";
+import rollupConfig from './rollup.config';
 
 const isDevMode = process.env.NODE_ENV !== 'production';
 
@@ -13,18 +13,18 @@ export default defineConfig(() => ({
     port: 4444,
     host: '0.0.0.0',
     watch: {
-      usePolling: true
-    }
+      usePolling: true,
+    },
   },
   build: {
     emptyOutDir: true,
     minify: !isDevMode,
     outDir: resolve(__dirname, 'dist'),
     sourcemap: isDevMode ? false : 'inline',
-    rollupOptions: rollupConfig
+    rollupOptions: rollupConfig,
   },
   css: {
-    devSourcemap: true
+    devSourcemap: true,
   },
 }));
 
